7d4ca0d0cae02d5afadc6482a696ca385f594721
memnochdavid <davduquediaz@gmail.com>
2024-11-12 17:24:51 +0100

    empezando con Compose

--- app/src/main/java/com/david/pokedex_pruebas/interfaz/ComposeActivity.kt
+++ app/src/main/java/com/david/pokedex_pruebas/interfaz/ComposeActivity.kt
@@ -7,13 +7,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.david.pokedex_pruebas.R
@@ -22,7 +33,15 @@ class ComposeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            saludo("David")
+            Row(Modifier
+                .background(colorResource(R.color.fuego))
+                .fillMaxSize()
+                .padding(8.dp)){
+                insertaImagen(R.drawable.charizard01)
+            }
+            Row{
+                saludo("Charizard")
+            }
         }
     }
 }
@@ -32,13 +51,21 @@ fun saludo(name: String) {
     Text("Hola $name!")
 }
 @Composable
-fun insertaImagen(draw: Drawable){
-
+fun insertaImagen(draw: Int){
+    Image(painter = painterResource(draw),"", alignment = Alignment.Center, contentScale = ContentScale.Fit)
 }
 
 
-@Preview(showBackground = true, widthDp = 200, heightDp = 200)
+@Preview(widthDp = 200, heightDp = 200)
 @Composable
-fun GreetingPreview2() {
-    Text("Hola polla!")
+fun setContent() {
+    Row(Modifier
+        .background(colorResource(R.color.fuego))
+        .fillMaxSize()
+        .padding(8.dp)){
+        insertaImagen(R.drawable.charizard01)
+    }
+    Row{
+        saludo("Charizard")
+    }
 }
